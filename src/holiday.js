ae9ed7b3b3fd9ef5df10ef38fd82cfb7591712a8
Alex Ian <alex.ian@sinlead.com>
2021-04-10 13:19:27 +0800

    Modify for spec

--- src/holiday.js
+++ src/holiday.js
@@ -2,6 +2,7 @@
 // 輸出是 output ： 'Merry christmas'/'Today is not christmas'
 // 情景是 senario ：判斷當天是否聖誕節
 // 實例化需求
+// 需求修改： 要 12/24 號也會顯示 'Merry christmas'
 
 export class Holiday {
   constructor() {
@@ -11,7 +12,8 @@ export class Holiday {
   sayHello() {
     const today = this.getToday();
 
-    if (`${today.getMonth() + 1}/${today.getDate()}` === this.christmasDay) {
+    // if (`${today.getMonth() + 1}/${today.getDate()}` === this.christmasDay) {
+    if (today.getMonth() + 1 === 12 && (today.getDate() === 25 || today.getDate() === 24)) {
       return 'Merry Xmas';
     }
 
--- test/holiday.test.js
+++ test/holiday.test.js
@@ -13,17 +13,27 @@ describe('check holiday sayHello', function () {
   });
 
   it('should return "Today is not Xmas"', () => {
+    givenDate(1, 24);
+    shouldReturn('Today is not Xmas');
+  });
+
+  it('should return "Today is not Xmas" at 11/24', () => {
     givenDate(11, 24);
     shouldReturn('Today is not Xmas');
   });
 
   it('should return "Merry Xmas"', () => {
-    givenDate(11, 25);
+    givenDate(12, 25);
+    shouldReturn('Merry Xmas');
+  });
+
+  it('should return "Merry Xmas" at 12/25', () => {
+    givenDate(12, 24);
     shouldReturn('Merry Xmas');
   });
 
   function givenDate(month, date) {
-    holiday.getToday = () => new Date(2020, month, date);
+    holiday.getToday = () => new Date(2020, month - 1, date);
   }
 
   function shouldReturn(msg) {
